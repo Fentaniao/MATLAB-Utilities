c111b65f088ab9add60fbbba8f2eb26587b4738c
Fentaniao <fentaniao@gmail.com>
2022-03-26 13:39:04 +0800

    Update logging in MuultiMlx2M

--- MWidgets/MultiMlx2M.m
+++ MWidgets/MultiMlx2M.m
@@ -3,7 +3,7 @@
 % MULTI-files edition
 
 % show start message
-disp(['MultiMlx2M start.']);
+disp(['MultiMlx2M starts.']);
 
 % cd to this file's path
 filePath = matlab.desktop.editor.getActiveFilename;
@@ -24,13 +24,19 @@ cd(fileDir);
 files = dir('*.mlx');
 nrfiles = numel(files);
 
-% transfer '.m' to '.mlx'
-for r = 1:nrfiles
-    str = files(r).name;
-    location = strfind(str, '.mlx');
-    filename = str(1:location(end) - 1);
-    matlab.internal.liveeditor.openAndConvert(str, [filename '.m']);
+if nrfiles > 0
+    % transfer '.m' to '.mlx'
+    for r = 1:nrfiles
+        str = files(r).name;
+        location = strfind(str, '.mlx');
+        filename = str(1:location(end)-1);
+        matlab.internal.liveeditor.openAndConvert(str, [filename, '.m']);
+        disp(['[Process: ', num2str(r), '/', num2str(nrfiles), '] Successful transfers "', filename, '" to .m.']);
+    end
+    
+    % show success message
+    disp(['MultiMlx2M successful transfers "', fileDir, '" to .m.']);
+else
+    % show no exit message
+    disp(['No .m file exits.']);
 end
-
-% show success message
-disp(['MultiMlx2M successful transfers "' fileDir '" to .m.']);
