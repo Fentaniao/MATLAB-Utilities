7db130f3e50779203b38c42a67e2bf428ebd90ff
Fentaniao <fentaniao@gmail.com>
2023-02-15 21:46:04 +0800

    Add a new widget for Live Script customize

---
+++ MWidgets/LiveScriptCustomize.m
@@ -0,0 +1,7 @@
+s = settings;
+
+% Live Editor code font name
+s.matlab.fonts.editor.code.Name.PersonalValue = 'Jetbrains Mono';
+
+% Live Editor normal font name
+s.matlab.fonts.editor.normal.Name.PersonalValue = 'Jetbrains Mono';
