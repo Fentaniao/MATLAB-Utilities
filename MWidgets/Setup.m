c61f73c790a3d500c65b6c7d51bc7e7ca94915c1
Fentaniao <62752455+Fentaniao@users.noreply.github.com>
2023-08-16 22:19:32 +0800

    Add custom icons for widgets

--- MWidgets/Setup.m
+++ MWidgets/Setup.m
@@ -1,6 +1,10 @@
+AutoCdPath;
+
 Labels = ["AutoCdPath", "OneMlx2M", "MultiMlx2M", "Beautifier"];
 Codes = ["AutoCdPath", "OneMlx2M", "MultiMlx2M", "Beautifier"];
-IconNames = ["favorite_command_A", "favorite_command_O", "favorite_command_M", "favorite_command_B"];
+Path = what("..\icons\").path;
+IconNames = ["move-to-folder.png", "circle-1.png", "code-compare.png", "indent.png"];
+% IconNames = ["favorite_command_A", "favorite_command_O", "favorite_command_M", "favorite_command_B"];
 
 fc = com.mathworks.mlwidgets.favoritecommands.FavoriteCommands.getInstance();
 % methodsview(fc)     % to show all available methods with their parameters
@@ -14,6 +18,7 @@ for ii = length(Labels):-1:1
     newFavoriteCommand.setCategoryLabel("WIDGETS");
     newFavoriteCommand.setCode(Code);
     newFavoriteCommand.setIconName(IconName);
+    newFavoriteCommand.setIconPath(Path);
     newFavoriteCommand.setIsOnQuickToolBar(true);
     newFavoriteCommand.setIsShowingLabelOnToolBar(true);
     % add the command to the favorite commands (the category is automatically created if it doesn't exist)
